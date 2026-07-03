c596471b17d40cf5d12872fcd5213afebccbdede
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 07:34:06 +0530

    added varaibles

---
+++ 03-var-coversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=suresh # NO space between values and =
+PERSON2=ramesh # NO space between values and =
+
+echo "$PERSON1: Hello $PERSON2,how r u?"
+echo "$PERSON2: hi $PERSON1 i am fine thanks for asking.how r u doing?"
+echo "$PERSON1: doing fine. what r u learning?"
+echo "$PERSON2: i am learning shell.what about u?"
