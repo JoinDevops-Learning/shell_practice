07ed61663abfecaf7b08b0795c2b5c1aa5b73c0d
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 09:09:31 +0530

    added time

--- 06-dynamic-var.sh
+++ 06-dynamic-var.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
-echo "script executed at $TIMESTAMP"
+START_TIME=$(date +%s) #script start time
+echo "script executed at $START_TIME"
+
+sleep 10 # simulate some processing time
+
+END_TIME=$(date +%s) #script end time
+echo "script executed at $END_TIME"
+
+echo "script executed at $START_TIME and ended at $END_TIME"
+
+EXECUTION_TIME=$(( END_TIME - START_TIME ))
+echo "script execution time is $EXECUTION_TIME seconds"
