32e87179f07b0e60435e6526e81ef254e026baeb
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 07:22:14 +0530

    added coversation script

---
+++ 02-coversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "suresh: Hello ramesh,how r u?"
+echo "ramesh: hi suresh i am fine thanks for asking.how r u doing?"
+echo "suresh: doing fine. what r u learning?"
+echo "ramesh: i am learning shell.what about u?"
