b550ef6396e3fe8236f8e8e3b2bbe3841ae5ed8a
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 08:54:19 +0530

    added password

--- 05-var-read.sh
+++ 05-var-read.sh
@@ -1,3 +1,8 @@
 echo "please enter username:"
 read USER_NAME
 echo "user name is $USER_NAME"
+echo "please enter username:"
+read USER_NAME
+echo "user name is $USER_NAME"
+echo "please enter u r password:"
+read -s PASSWORD # not visible
