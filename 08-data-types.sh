8e8ab6ac5c953ed5ff9a89d13ffa5614e6b0a7f3
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 09:43:19 +0530

    added string instead of number

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # no need to type datatype in shell script. 
 #shell will automatically identify the datatype of variable based on the value assigned to it.
-NUM1=10
+NUM1=balu
 NUM2=20
 SUM=$((NUM1 + NUM2))
 echo "sum of $NUM1 and $NUM2 is $SUM"
