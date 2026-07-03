9b0609cbfe0ce88d827c5f8e21aea57ce0b210c4
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 08:45:19 +0530

    added arguments

---
+++ 04-arg-var-conver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1 # NO space between values and =
+PERSON2=$2 # NO space between values and =
+
+echo "$PERSON1: Hello $PERSON2,how r u?"
+echo "$PERSON2: hi $PERSON1 i am fine thanks for asking.how r u doing?"
+echo "$PERSON1: doing fine. what r u learning?"
+echo "$PERSON2: i am learning shell.what about u?"
