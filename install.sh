d0aa4621b0cb3135232b05901fb2e23d048763d6
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 10:17:41 +0530

    added installation script with exit code

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]; then  
+    echo "please run this script as root user"
+    exit 1
+fi
+echo "installing nginx web server"
+dnf install -y nginx
