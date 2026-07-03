3a99e5ec36cbe8805842fd36d871d37e538ea828
balu676 <balarajuannaldevu010@gmail.com>
2026-07-03 08:56:17 +0530

    file changed

--- 05-var-read.sh
+++ 05-var-read.sh
@@ -1,8 +1,6 @@
 echo "please enter username:"
 read USER_NAME
 echo "user name is $USER_NAME"
-echo "please enter username:"
-read USER_NAME
-echo "user name is $USER_NAME"
+
 echo "please enter u r password:"
 read -s PASSWORD # not visible
