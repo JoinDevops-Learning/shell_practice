50f54596e9e87c995b78bb0aa3a553ceb1376fbc
balu676 <balarajuannaldevu010@gmail.com>
2026-07-05 12:39:59 +0530

    added colours

---
+++ 17-colours.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+R="\e[31m" #RED
+G="\e[32m" #GREEN
+Y="\e[33m" #YELLOW
+B="\e[34m" #BLUE
+M="\e[35m" #MAGENTA
+C="\e[36m" #CYAN  
+N="\e[0m"  #NO COLOR  
+
+echo -e "$R hello world $N, $Y i am leaning linux $N, $G i am learning shell scripting $N, $B i am learning bash scripting $N, $M i am learning shell programming $N, $C i am learning bash programming $N"
