11a057013fe97a199c9c542246eed708e3f7f2a8
balu676 <balarajuannaldevu010@gmail.com>
2026-07-05 10:38:36 +0530

    added instllations with exit code

---
+++ 10-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then  
+    echo "please run this script as root user"
+    exit 1
+fi
+echo "installing nginx web server"
+dnf install -y nginx
+
+if [ $? -ne 0 ]; then
+    echo "nginx installed failed"
+    exit 1
+else
+    echo "nginx installation success"
+fi
+
+dnf install mysql-server -y
+if [ $? -ne 0 ]; then
+    echo "nginx installed failed"
+    exit 1
+else
+    echo "nginx installation success"
+fi
+
+dnf install nodejs -y
+if [ $? -ne 0 ]; then
+    echo "nginx installed failed"
+    exit 1
+else
+    echo "nginx installation success"
+fi
+
+
