212e78cd572be33354b2a5f1067861fdfa8d65b7
balu676 <balarajuannaldevu010@gmail.com>
2026-07-05 11:32:41 +0530

    added logs to the script

---
+++ 12-logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell_script"
+LOGS_FILE="/var/log/shell_script/$0.log"
+
+if [ $USERID -ne 0 ]; then  
+    echo "please run this script as root user"
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+    echo "$2 ... failed"
+    exit 1
+else
+    echo "$2 ... success"
+fi
+}
+echo "installing nginx web server"
+
+dnf install -y nginx & >> $LOGS_FILE
+VALIDATE $? "installing nginx"
+
+dnf install mysql-server -y & >> $LOGS_FILE
+VALIDATE $? "installing mysql-server"
+
+dnf install nodejs -y & >> $LOGS_FILE
+VALIDATE $? "installing nodejs"
+
+
