44ee477f6d72aa27a29ba8467e9ae488a87eb92b
balu676 <balarajuannaldevu010@gmail.com>
2026-07-05 12:19:21 +0530

    added loop skipping

---
+++ 16-install-loop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then  
+    echo "please run this script as root user" | tee -a $LOGS_FILE
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+    echo "$2 ... failed" | tee -a $LOGS_FILE # -a append the logs to the file
+    exit 1
+else
+    echo "$2 ... success" | tee -a $LOGS_FILE
+fi
+}
+
+for package in $@ # sudo sh 15-logs-loops.sh nginx mysql-server nodejs
+do
+    dnf install -y $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now" | tee -a $LOGS_FILE
+        dnf install -y $package &>>$LOGS_FILE
+        VALIDATE $? "installing $package"
+    else
+        echo "$package already installed skipping" | tee -a $LOGS_FILE
+    fi
+    
+
+done
+
+
